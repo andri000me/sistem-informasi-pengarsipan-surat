a16b275825287f42726e4a76af1c67b6520271ee
Marzuki <marzukiberg@gmail.com>
2020-04-21 18:43:10 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://localhost/e-arsip_sb/';
-$config['base_url'] = 'http://localhost/e-arsip-ci/';
+$config['base_url'] = 'http://sisarsip-smkdh.now.sh';
 
 /*
 |--------------------------------------------------------------------------
